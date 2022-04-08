7d6a97a8bba6dacf4575bda6e8b8cf60334525ce
yueshen <yue.shen@nokia-sbell.com>
2022-04-08 14:43:50 +0800

    update: init the python3 code for leetcode problem 1&2

---
+++ leetCode/problems/2.两数相加.py
@@ -0,0 +1,34 @@
+# @before-stub-for-debug-begin
+from python3problem2 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode.cn id=2 lang=python3
+#
+# [2] 两数相加
+#
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = curr = ListNode()
+        carry = val = 0
+
+        while carry or l1 or l2:
+            val = carry
+
+            if l1: l1, val = l1.next, l1.val + val
+            if l2: l2, val = l2.next, l2.val + val
+
+            carry, val = divmod(val, 10)
+            curr.next = curr = ListNode(val)
+
+        return head.next
+
+# @lc code=end
+
